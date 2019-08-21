e8d48f344c421183ec06c4a1d178d3e0eb381f5c
AshwinChandlapur <ashwinchandlapur@gmail.com>
2019-08-21 18:19:36 -0400

    Ashwin Trail

--- app/src/main/java/vadeworks/nannyroo/MainActivity.java
+++ app/src/main/java/vadeworks/nannyroo/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity  {
                 startActivity(intent);
             }
         });
+
+        //Ashwin Trail Commit
     }
 }
 //change again
